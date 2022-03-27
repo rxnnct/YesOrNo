e6a1eaf2ea782f1cf54902c8d604dbaff777b756
Rixen <rixennoct@rambler.ru>
2022-03-27 20:53:50 +0300

    Bottom navigation added

---
+++ app/src/main/java/ru/rxnnct/yesorno/screens/MainMenuFragment.kt
@@ -0,0 +1,13 @@
+package ru.rxnnct.yesorno.screens
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import ru.rxnnct.yesorno.R
+
+class MainMenuFragment: Fragment(R.layout.fragment_main_menu) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
--- app/src/main/java/ru/rxnnct/yesorno/screens/TutorialFragment.kt
+++ app/src/main/java/ru/rxnnct/yesorno/screens/TutorialFragment.kt
@@ -2,7 +2,11 @@ package ru.rxnnct.yesorno.screens
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 import ru.rxnnct.yesorno.R
 
 class TutorialFragment : Fragment(R.layout.fragment_tutorial) {
@@ -10,6 +14,22 @@ class TutorialFragment : Fragment(R.layout.fragment_tutorial) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // TODO: button
+        view.findViewById<Button>(R.id.agreeButton).setOnClickListener {
+            findNavController().navigate(
+                R.id.action_tutorialFragment_to_mainFragment,
+                bundleOf(),
+                navOptions {
+                    anim {
+                        enter = androidx.navigation.ui.R.anim.nav_default_enter_anim
+                        popEnter = androidx.navigation.ui.R.anim.nav_default_pop_enter_anim
+                        popExit = androidx.navigation.ui.R.anim.nav_default_pop_exit_anim
+                        exit = androidx.navigation.ui.R.anim.nav_default_exit_anim
+                    }
+                    launchSingleTop = true
+                    popUpTo(R.id.navigation_graph_application) {
+                        inclusive = true
+                    }
+                })
+        }
     }
 }
