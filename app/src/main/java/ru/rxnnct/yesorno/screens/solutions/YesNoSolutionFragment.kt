a317a6bf04dff14cb2802bbdc3edad7dc2aa19a7
Rixen <rixennoct@rambler.ru>
2022-03-30 20:02:54 +0300

    Solutions added

--- app/src/main/java/ru/rxnnct/yesorno/screens/solutions/YesNoSolutionFragment.kt
+++ app/src/main/java/ru/rxnnct/yesorno/screens/solutions/YesNoSolutionFragment.kt
@@ -1,11 +1,11 @@
-package ru.rxnnct.yesorno.screens
+package ru.rxnnct.yesorno.screens.solutions
 
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import ru.rxnnct.yesorno.R
 
-class MainMenuFragment: Fragment(R.layout.fragment_main_menu) {
+class YesNoSolutionFragment: Fragment(R.layout.fragment_yes_no_solution) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
