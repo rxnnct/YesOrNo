49179786ea8904c12d119b3d77162b838d33abd5
Rixen <rixennoct@rambler.ru>
2022-04-03 18:39:16 +0300

    ViewModel added

--- app/src/main/java/ru/rxnnct/yesorno/screens/solutions/YesNoSolutionFragment.kt
+++ app/src/main/java/ru/rxnnct/yesorno/screens/solutions/YesNoSolutionFragment.kt
@@ -1,13 +1,19 @@
 package ru.rxnnct.yesorno.screens.solutions
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import ru.rxnnct.yesorno.R
 
 class YesNoSolutionFragment: Fragment(R.layout.fragment_yes_no_solution) {
 
+    private lateinit var viewModel: YesNoSolutionViewModel
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(YesNoSolutionViewModel::class.java)
     }
 }
---
+++ app/src/main/java/ru/rxnnct/yesorno/screens/solutions/YesNoSolutionViewModel.kt
@@ -0,0 +1,13 @@
+package ru.rxnnct.yesorno.screens.solutions
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+class YesNoSolutionViewModel : ViewModel() {
+    init {
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+    }
+}
