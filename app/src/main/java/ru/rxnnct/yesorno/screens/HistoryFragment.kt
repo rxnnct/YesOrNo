3f64c433381e40bd4dd586f2dc99fc24478aa7e1
Rixen <rixennoct@rambler.ru>
2022-04-19 22:12:24 +0300

    RecyclerView for history fragment

--- app/src/main/java/ru/rxnnct/yesorno/screens/HistoryFragment.kt
+++ app/src/main/java/ru/rxnnct/yesorno/screens/HistoryFragment.kt
@@ -3,11 +3,18 @@ package ru.rxnnct.yesorno.screens
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import ru.rxnnct.yesorno.R
 
 class HistoryFragment: Fragment(R.layout.fragment_history) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.historyRecyclerview)
+        val adapter = HistoryListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(context)
     }
 }
