16fe9ffa4752bdfc977c039e570bf703fe942e94
Rixen <rixennoct@rambler.ru>
2022-04-29 23:14:05 +0300

    Splash fix

--- app/src/main/java/ru/rxnnct/yesorno/screens/SplashFragment.kt
+++ app/src/main/java/ru/rxnnct/yesorno/screens/SplashFragment.kt
@@ -4,19 +4,27 @@ import android.os.Bundle
 import android.view.View
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.navOptions
 import ru.rxnnct.yesorno.MainActivity
 import ru.rxnnct.yesorno.R
+import ru.rxnnct.yesorno.YesOrNoApplication
+import ru.rxnnct.yesorno.model.Settings
 
 class SplashFragment : Fragment(R.layout.fragment_splash) {
+
+    private val settingsViewModel: SettingsViewModel by viewModels {
+        SettingsViewModelFactory((activity?.application as YesOrNoApplication).settingsRepository)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         view.postDelayed({
+            val appSettings = settingsViewModel.settings
 
-            val action: Int = if (MainActivity.isTutorialEnded == "true") {
+            val action: Int = if (appSettings.value?.tutorialCompleted == true) {
                 R.id.action_splashFragment_to_mainFragment
             } else {
                 R.id.action_splashFragment_to_tutorialFragment
